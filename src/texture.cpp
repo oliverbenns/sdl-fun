48351d11595e928238414f24ccaaa4f9b94d7e31
Oliver Benns <oliverbenns@gmail.com>
2017-10-04 16:50:19 +0100

    Remove unneeded cpp string from path

--- src/texture.cpp
+++ src/texture.cpp
@@ -3,19 +3,19 @@
 #include <string>
 #include "texture.h"
 
-SDL_Texture* loadTexture(SDL_Renderer * renderer, std::string path) {
+SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path) {
   //The final texture
   SDL_Texture* newTexture = NULL;
   //Load image at specified path
-  SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+  SDL_Surface* loadedSurface = SDL_LoadBMP(path);
 
   if (loadedSurface == NULL) {
-    printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), SDL_GetError() );
+    printf( "Unable to load image %s! SDL_image Error: %s\n", path, SDL_GetError() );
   } else {
     //Create texture from surface pixels
     newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
     if (newTexture == NULL) {
-      printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+      printf( "Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError() );
     }
 
     //Get rid of old loaded surface
--- src/texture.h
+++ src/texture.h
@@ -1,6 +1,6 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-SDL_Texture* loadTexture(SDL_Renderer * renderer, std::string path);
+SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path);
 
 #endif
