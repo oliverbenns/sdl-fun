10fbd8d5ea9be0ddca9a4cb67dc2926cb5b0dc45
Oliver Benns <oliverbenns@gmail.com>
2017-10-04 16:18:35 +0100

    Remove test code and render button

--- src/main.cpp
+++ src/main.cpp
@@ -12,45 +12,22 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-enum KeyPressSurfaces : char {
-  KEY_PRESS_SURFACE_DEFAULT,
-  KEY_PRESS_SURFACE_UP,
-  KEY_PRESS_SURFACE_DOWN,
-  KEY_PRESS_SURFACE_LEFT,
-  KEY_PRESS_SURFACE_RIGHT,
-  KEY_PRESS_SURFACE_TOTAL
-};
-
-//Starts up SDL and creates window
 bool init();
-
-//Loads media
 bool loadMedia();
-
-//Frees media and shuts down SDL
 void close();
 
-//Loads individual image
-SDL_Surface* loadSurface( std::string path );
-
-//The surface contained by the window
-SDL_Texture* gScreenSurface = NULL;
-
-//The images that correspond to a keypress
-SDL_Texture* gKeyPressSurfaces[ KEY_PRESS_SURFACE_TOTAL ];
-
-//Current displayed image
-SDL_Texture* gCurrentTexture = NULL;
-
 WindowContext context;
+SDL_Texture* buttonTexture;
+SDL_Rect srcR;
+SDL_Rect destR;
 
 bool init() {
-  //Initialization flag
+  // Initialization flag
   bool success = true;
 
-  //Initialize SDL
-  if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-    printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+  // Initialize SDL
+  if(SDL_Init( SDL_INIT_VIDEO ) < 0) {
+    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     success = false;
   } else {
     createWindowContext(&context, 800, 600);
@@ -59,52 +36,19 @@ bool init() {
   return success;
 }
 
-bool loadMedia()
-{
-    //Loading success flag
-    bool success = true;
+bool loadMedia() {
+  // Loading success flag
+  bool success = true;
 
-    //Load default surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] = loadTexture(context.renderer, "press.bmp");
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] == NULL )
-    {
-        printf( "Failed to load default image!\n" );
-        success = false;
-    }
+  //Load default surface
+  buttonTexture = loadTexture(context.renderer, "button.bmp");
 
-    //Load up surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] = loadTexture(context.renderer, "up.bmp");
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] == NULL )
-    {
-        printf( "Failed to load up image!\n" );
-        success = false;
-    }
+  if (buttonTexture == NULL) {
+    printf( "Failed to load button image!\n" );
+    success = false;
+  }
 
-    //Load down surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] = loadTexture(context.renderer, "down.bmp");
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] == NULL )
-    {
-        printf( "Failed to load down image!\n" );
-        success = false;
-    }
-
-    //Load left surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] = loadTexture(context.renderer, "left.bmp");
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] == NULL )
-    {
-        printf( "Failed to load left image!\n" );
-        success = false;
-    }
-
-    //Load right surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] = loadTexture(context.renderer, "right.bmp");
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] == NULL )
-    {
-        printf( "Failed to load right image!\n" );
-        success = false;
-    }
-
-    return success;
+  return success;
 }
 
 void close() {
@@ -113,68 +57,42 @@ void close() {
 }
 
 int main(int argc, char* args[]) {
-  //Start up SDL and create window
-  if( !init() )
-  {
+  // Start up SDL and create window
+  if(!init()) {
     printf( "Failed to initialize!\n" );
-  }
-  else
-  {
-    //Load media
-    if( !loadMedia() )
-    {
+  } else {
+    // Load media
+    if(!loadMedia()) {
       printf( "Failed to load media!\n" );
-    }
-    else
-    {
+    } else {
       // Main loop flag
       bool quit = false;
 
       // Event handler
       SDL_Event e;
 
-      while( !quit )
-      {
-        //Handle events on queue
-        while( SDL_PollEvent( &e ) != 0 )
-        {
+      while(!quit) {
+        // Handle events on queue
+        while( SDL_PollEvent(&e) != 0 ) {
           //User requests quit
-          if( e.type == SDL_QUIT )
-          {
+          if (e.type == SDL_QUIT) {
             quit = true;
           }
-          else if( e.type == SDL_KEYDOWN ) {
-            //Select surfaces based on key press
-            switch(e.key.keysym.sym) {
-              case SDLK_UP:
-              gCurrentTexture = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
-              break;
-
-              case SDLK_DOWN:
-              gCurrentTexture = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
-              break;
-
-              case SDLK_LEFT:
-              gCurrentTexture = gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ];
-              break;
-
-              case SDLK_RIGHT:
-              gCurrentTexture = gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ];
-              break;
-
-              default:
-              gCurrentTexture = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
-              break;
-            }
-
-          }
         }
+
+        int w;
+        int h;
+
+        SDL_QueryTexture(buttonTexture, NULL, NULL, &w, &h);
+
+        destR.h = h;
+        destR.w = w;
 
         //Clear screen
         SDL_RenderClear(context.renderer);
 
         //Render texture to screen
-        SDL_RenderCopy(context.renderer, gCurrentTexture, NULL, NULL);
+        SDL_RenderCopy(context.renderer, buttonTexture, NULL, &destR);
 
         //Update screen
         SDL_RenderPresent(context.renderer);
