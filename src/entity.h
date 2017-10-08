43439f6527d79e18aee972c80b81ecc1f5051e22
Oliver Benns <oliverbenns@gmail.com>
2017-10-08 20:22:58 +0100

    Add vector

--- src/entity.h
+++ src/entity.h
@@ -1,11 +1,11 @@
 #pragma once
 
-struct Entity {
+#include "vector.h"
+
+struct Entity : Vector {
   // @TODO: Do I need to define virtual deconstructor?
 
   // @TODO: Store position on entity or a position/transform component? Or on Sprite?
-  int x;
-  int y;
 
   unsigned int id;
   char tag; // @TODO: Allow multiple?
