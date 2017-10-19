dddc4a25fe93826b8c081b050b4711e24905d684
Oliver Benns <oliverbenns@gmail.com>
2017-10-19 13:37:52 +0100

    Update controls to WASD

--- src/components/input.cpp
+++ src/components/input.cpp
@@ -5,15 +5,15 @@
 Input::Input() {
   state = SDL_GetKeyboardState(NULL);
 
-  up = &state[SDL_SCANCODE_UP];
-  down = &state[SDL_SCANCODE_DOWN];
-  left = &state[SDL_SCANCODE_LEFT];
-  right = &state[SDL_SCANCODE_RIGHT];
+  up = &state[SDL_SCANCODE_W];
+  down = &state[SDL_SCANCODE_S];
+  left = &state[SDL_SCANCODE_A];
+  right = &state[SDL_SCANCODE_D];
 }
 
 // @TODO: This is obviously specific right now. How can I make this more general? If possible?
 void Input::update(Entity& entity, double deltaTime) {
-  if (*up != 0) {
+  if (*up) {
     entity.y -= deltaTime;
   }
 
