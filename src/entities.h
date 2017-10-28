81c3320dba0b42ef5d5a400c13f398679dd21748
Oliver Benns <oliverbenns@gmail.com>
2017-10-28 17:31:02 +0100

    Fix build

--- src/entities.h
+++ src/entities.h
@@ -9,19 +9,18 @@
 struct Entity;
 
 struct Player : Entity {
-  Player(SDL_Renderer* renderer) : a(5) {
-    this->addComponentTest(new Body());
+  Player(SDL_Renderer* renderer) {
+    this->addComponent(new Body());
     this->addComponent(new Input());
     this->addComponent(new Sprite(renderer, "player.bmp"));
-      
-      
+
+
   }
-  int a = 3;
 };
 
 struct Floor : Entity {
   Floor(SDL_Renderer* renderer) {
-    this->addComponentTest(new Body());
+    this->addComponent(new Body());
     this->addComponent(new Sprite(renderer, "floor.bmp"));
 
     this->body->gravity = false;
